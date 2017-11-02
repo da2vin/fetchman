b4baaa976bb05f685f5fe12205c3254de0e8d180
DarkSand <darkwings_love@163.com>
2017-11-02 10:13:35 +0800

    fetchman better man

--- tests/test_processor.py
+++ tests/test_processor.py
@@ -11,6 +11,6 @@ from fetchman.pipeline.test_pipeline import TestPipeline
 class TestProcessor(unittest.TestCase):
     def test_car_processor(self):
         test_pipeline = TestPipeline()
-        SpiderCore(TEST_Processor(),test=True).set_pipeline('console',ConsolePipeline()).set_pipeline('save',PicPipeline())\
-            .set_pipeline('test',test_pipeline).start()
+        SpiderCore(TEST_Processor(),test=True).set_pipeline(ConsolePipeline(),'console').set_pipeline(PicPipeline(),'save')\
+            .set_pipeline(test_pipeline,'test').start()
         self.assertIn('2017',test_pipeline.result['date_time'])
