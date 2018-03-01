c0f71182209813bae7092520181d09b8df1bd16d
DarkSand <darkwings_love@163.com>
2018-03-01 11:17:53 +0800

    fix bugs

--- fetchman/processor/test_processor.py
+++ fetchman/processor/test_processor.py
@@ -5,9 +5,7 @@ from fetchman.processor.base_processor import BaseProcessor
 from fetchman.downloader.http.spider_request import Request
 from fetchman.utils.decorator import check
 from fetchman.pipeline.console_pipeline import ConsolePipeline
-from fetchman.pipeline.pic_pipeline import PicPipeline
 from fetchman.pipeline.pipe_item import pipeItem
-from fetchman.pipeline.test_pipeline import TestPipeline
 
 from bs4 import BeautifulSoup as bs
 import hashlib
@@ -21,67 +19,73 @@ if sys.version_info < (3, 0):
 
 
 
-class TEST_Processor(BaseProcessor):
-    spider_id = 'zhu_spider'
-    allowed_domains = ['zhuwang.cc']
-    start_requests = [Request(url='http://www.zhuwang.cc/list-58-1.html', priority=0)]
+# 爬取土流网解析器
+class Tuliu_Processor(BaseProcessor):
+    spider_id = 'tuliu_spider'
+    allowed_domains = ['tuliu.com']
+
+    # 推入初始request
+    @classmethod
+    def init_start_requests(cls):
+        cls.start_requests.extend([Request(url='http://www.tuliu.com/news/list-c165/%s.html' % page, priority=0, meta={'newsCateId': '20171102111907007'}) for page in range(1, 9)])
+        cls.start_requests.extend([Request(url='http://www.tuliu.com/news/list-c163/%s.html' % page, priority=0, meta={'newsCateId': '20171102111907007'}) for page in range(1, 30)])
 
     @check
     def process(self, response):
-        soup = bs(response.m_response.content, 'lxml')
+        if '404错误' not in response.m_response.content:
+            soup = bs(response.m_response.content, 'lxml')
 
-        page_list = soup.select('div.zxpage a')
-        total_page = int(page_list[page_list.__len__()-2].text)
-        page = 1
-        while page<=total_page:
-            yield Request(url='http://www.zhuwang.cc/list-58-%d.html' % page,callback=self.process_page, priority=0,duplicate_remove=False)
-            page +=1
+            tuliu_div_list = soup.select('div.news_list_list ul li.list_box')
+            for tuliu_div in tuliu_div_list:
+                if tuliu_div.select('a img'):
+                    detail_url = tuliu_div.select('a')[0]['href']
+                    img_url = tuliu_div.select('a img')[0]['src']
+                    name = tuliu_div.select('h1.category_title nobr.l')[0].text.strip()
+                    createTime = tuliu_div.select('h1.category_title nobr.r')[0].text.replace('发布时间 ', '').strip()
+                    shortDes = tuliu_div.select('div')[0].text.replace('[查看全文]', '')
 
-    @check
-    def process_page(self, response):
-        soup = bs(response.m_response.content, 'lxml')
+                    md5 = hashlib.md5()
+                    rand_name = str(time.time()) + str(random.random())
+                    md5.update(rand_name)
+                    img_name = md5.hexdigest() + '.jpg'
 
-        zhu_div_list = soup.select('div.zxleft ul li')
-        for zhu_div in zhu_div_list:
-            detail_url = zhu_div.select('a')[0]['href']
-            img_url = zhu_div.select('a img')[0]['src']
-            title = zhu_div.select('a img')[0]['alt'].strip()
-            shortDes = zhu_div.select('p.zxleft32 a')[0].text
+                    request = Request(url=detail_url, priority=1, callback=self.process_detail)
+                    request.meta['name'] = name
+                    request.meta['createTime'] = createTime
+                    request.meta['shortDes'] = shortDes
+                    request.meta['img_name'] = img_name
+                    request.meta['newsCateId'] = response.request.meta['newsCateId']
+                    yield request
 
-            md5 = hashlib.md5()
-            rand_name = str(time.time()) + str(random.random())
-            md5.update(rand_name.encode(encoding='utf-8'))
-            img_name = md5.hexdigest() + '.jpg'
-
-            request = Request(url=img_url, priority=1, callback=self.process_pic)
-            request.meta['img_name'] = img_name
-            yield request
-
-            request = Request(url=detail_url, priority=1, callback=self.process_detail)
-            request.meta['title'] = title
-            request.meta['shortDes'] = shortDes
-            request.meta['img_name'] = img_name
-            yield request
-
-    @check
-    def process_pic(self, response):
-        result = response.m_response.content
-        yield pipeItem(['save'],result)
-
+    # 获取新闻详情并丢入DataBasePipeline
     @check
     def process_detail(self, response):
         soup = bs(response.m_response.content, 'lxml')
-
-        dd_tail = soup.select('div.zxxwleft p.zxxw2')[0].text.replace('来源： ','').replace('来源：','').split(' ')
-        date_time = dd_tail[1].strip() + ' ' + dd_tail[2].strip().replace('|','')
-        newsFrom = dd_tail[0].strip()
-
         result = dict()
-        result['date_time'] = date_time
-        result['newsFrom'] = newsFrom
+        result['newsProductId'] = time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))
+        result['newsCateId'] = response.request.meta['newsCateId']
+        result['name'] = response.request.meta['name']
+        result['imageUrl'] = response.request.meta['img_name']
+        result['newsCateId'] = response.request.meta['newsCateId']
+        result['shortDes'] = response.request.meta['shortDes']
+        result['createTime'] = response.request.meta['createTime']
+        result['newsFromWebUrl'] = response.request.url
+        span_list = soup.select('div.article-header p.text-gray-9 span')
+        for span in span_list:
+            if '来源：' in span.text:
+                result['newsFrom'] = span.text.replace('来源：', '').strip()
+                break
+            else:
+                result['newsFrom'] = '互联网'
+        longDes = soup.select('div.article-content')[0]
 
-        yield pipeItem(['console','test'],result)
+        result['longDes'] = str(longDes)
+
+        yield pipeItem(['console'], result)
+
 
 if __name__ == '__main__':
-    SpiderCore(TEST_Processor()).set_pipeline(ConsolePipeline(),'console')\
-        .set_pipeline(PicPipeline(),'save').set_pipeline(TestPipeline(),'test').start()
+    # 生成爬虫对象，设置pipeline，启动爬虫
+    SpiderCore(Tuliu_Processor(),time_sleep=1) \
+        .set_pipeline(ConsolePipeline(), 'console') \
+        .start()
--- tests/test_processor.py
+++ tests/test_processor.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import unittest2 as unittest
-from fetchman.processor.test_processor import TEST_Processor
+from fetchman.processor.test_processor import Tuliu_Processor
 from fetchman.spider.spider_core import SpiderCore
 from fetchman.pipeline.console_pipeline import ConsolePipeline
-from fetchman.pipeline.pic_pipeline import PicPipeline
 from fetchman.pipeline.test_pipeline import TestPipeline
 
 
 class TestProcessor(unittest.TestCase):
     def test_car_processor(self):
         test_pipeline = TestPipeline()
-        SpiderCore(TEST_Processor(),test=True).set_pipeline(ConsolePipeline(),'console').set_pipeline(PicPipeline(),'save')\
+        SpiderCore(Tuliu_Processor(),test=True).set_pipeline(ConsolePipeline(),'console')\
             .set_pipeline(test_pipeline,'test').start()
         self.assertIn('2018',"2018-01-07 09:40:39")
